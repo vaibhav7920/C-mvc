13d4bafc9bbe6e4a18795a7b532fc5c3ad02c69c
Vaibhav <42677589+vaibhav7920@users.noreply.github.com>
2023-03-20 14:51:05 +0530

    11

---
+++ database/database/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace database.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
