39c37bf169ab54a153eaacc63ee6b86c8fb1a64d
Vaibhav <42677589+vaibhav7920@users.noreply.github.com>
2023-03-23 13:11:39 +0530

    11

---
+++ Employeedatabase/Employeedatabase/Models/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Employeedatabase.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
